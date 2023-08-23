7057cca5863fd81cb174f58693f960c6db2fe64a
Eve Landin <evelandin@Eves-MacBook-Pro.local>
2023-08-23 13:33:40 +0100

    identify note name from midi number

---
+++ music_theory.py
@@ -0,0 +1,18 @@
+C4 = 60
+C5 = C4 + 12
+def pitch_class(pitch):
+	return pitch % 12
+print (pitch_class(100))
+
+def note_name(midi_number):
+	
+
+	chromatic_scale_sharps = ["C", "C#", "D", 
+"D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
+
+	return chromatic_scale_sharps[pitch_class(midi_number)]
+	
+	
+print (note_name(69))
+
+
